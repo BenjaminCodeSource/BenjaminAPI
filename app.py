e86113661cb2dba8eb1bf30380c3e48ca578d90d
BenjaminCodeSource <79181497+BenjaminCodeSource@users.noreply.github.com>
2021-02-17 11:51:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,6 @@ def spam(benjamin):
   data={"username": benjamin}
   spamx = requests.post("https://cmsapi.mapclub.com/api/reset-password-otp",headers=abin,json=data).text
   return "SPAM SMS MAP CLUB  CREATOR : BENJAMIN-CODE"
-
-def sms(benjamin):
-  spam()
-  no = str(benjamin)
-  data={"username": benjamin}
-  wasap = requests.post("https://cmsapi.mapclub.com/api/reset-password-otp",headers=abin,json=data)
-  bin = wasap.text
-  return "SPAM SMS MAP CLUB BY BENJAMIN CODE"
 if __name__=='__main__':
  app.run(debug=True)
 
