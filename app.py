cecdcaa995842af47a9fcdca786c7dba56139621
BenjaminCodeSource <79181497+BenjaminCodeSource@users.noreply.github.com>
2021-02-17 11:55:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 import jsonify, requests, json, time
 
-abin={
-"Host": "cmsapi.mapclub.com"}
+
 app = Flask(__name__)
 @app.route('/<benjamin>', methods=['GET', 'POST'])
 def spam(benjamin):
-  data={"username": benjamin}
-  spamx = requests.post("https://cmsapi.mapclub.com/api/reset-password-otp",headers=abin,json=data).text
-  return "SPAM SMS MAP CLUB  CREATOR : BENJAMIN-CODE"
+  return(benjamin)
 if __name__=='__main__':
  app.run(debug=True)
 
